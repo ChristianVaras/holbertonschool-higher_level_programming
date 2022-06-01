910dfb38c25d6c19bae0c8d19ccb373b81d3e57e
Christian Varas <christianvaras.2020@gmail.com>
2022-05-31 21:28:03 -0500

    Write a function that returns an object (Python data structure) represented by a JSON string for task 4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,7 +5,6 @@ Contains function that returns JSON representation of obj (string)
 import json
 
 
-
 def to_json_string(my_obj):
     """function that returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
