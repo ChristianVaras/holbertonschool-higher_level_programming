73e38525b3c5a38f4b8408bb9807cd08c62c56e0
Christian Varas <christianvaras.2020@gmail.com>
2022-05-31 23:11:28 -0500

    Write a function that returns the dictionary description with simple data structure for JSON serialization of an object for task 8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""
-Contains function that adds and saves to Python obj to JSON file; loads objects
+"""Contains function that adds and saves to Python obj
+to JSON file; loads objects
 """
 import json
 from sys import argv
@@ -12,12 +12,14 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 # Create the object or calling from json
 try:
+    f = open(filename, 'r')
     obj = load_from_json_file(filename)
-except FileNotFoundError:
+except Exception:
     obj = []
 
-finally:
-    for i in range(1, len(argv)):
+for i in range(1, len(argv)):
     obj.append(argv[i])
-    # Create json file
-    save_to_json_file(obj, filename)
+
+# Create json file
+with open(filename, 'w') as f:
+    json.dump(obj, f)
