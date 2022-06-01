91b29601ead9df78f44bcc5f2d2f14fccca5c63c
Christian Varas <christianvaras.2020@gmail.com>
2022-05-31 23:02:59 -0500

    Fix the code for task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,17 +2,22 @@
 """
 Contains function that adds and saves to Python obj to JSON file; loads objects
 """
-
-
+import json
 from sys import argv
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-
+# Create the object or calling from json
 try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    pass
+    obj = load_from_json_file(filename)
+except Exception:
+    obj = []
 
-save_to_json_file(existing_content + argv[1:], filename)
+for i in range(1, len(argv)):
+    obj.append(argv[i])
+
+# Create json file
+save_to_json_file(obj, filename)
