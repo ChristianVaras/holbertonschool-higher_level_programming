d74ab23da9323a9c57c53426af6b8446ff240be5
Christian Varas <christianvaras.2020@gmail.com>
2022-07-06 23:39:11 -0500

    Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
 -- lists all cities
-SELECT cities.id, cities.name, states.name
+SELECT c.id, c.name, s.name
     FROM cities AS c
     JOIN states AS s
     WHERE c.state_id = s.id
