981a5eeb87d7bd83fd229d59fcdd32ce2f3c7fa7
Christian Varas <christianvaras.2020@gmail.com>
2022-08-05 00:03:10 -0500

    Write a function that prints the number of arguments already printed and the new argument value.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 //  class Square that defines a square and inherits from Square of 5-square.js
-const SquarePrev = require('./5-square');
+const Rectangle = require('./4-rectangle');
 
-class Square extends SquarePrev {
+class Square extends Rectangle {
   charPrint (c) {
     const x = !c ? 'X' : c;
     for (let i = 0; i < this.height; i++) {
