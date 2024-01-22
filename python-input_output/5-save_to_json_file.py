25423571e641e4bd3eef8ac6209a1874d00e8665
ChristianVaras <christianvaras.2020@gmail.com>
2024-01-22 15:57:38 -0500

    Fixing code

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""
-Module 7-save_to_json_file
-
-Contains function that writes Python obj to file using JSON represenation
-"""
+"""Function save Object to a File"""
+import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Writes Python obj to file using JSON represenation
-    Args:
-        my_obj: python object
-        filename: file
-    """
-    import json
+    """Function that writes an Object to a text file
+    using JSON representation"""
 
     with open(filename, mode="w", encoding="utf-8") as f:
-        json.dump(my_obj, f)
+        f.write(json.dumps(my_obj))
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""
-Module 8-load_from_json_file
-
-Contains function that creates a Python obj from JSON file
-"""
+"""Create object from a JSON file"""
+import json
 
 
 def load_from_json_file(filename):
-    """Creates a Python obj from JSON file
-    Args:
-        filename: file
-    """
-    import json
-
-    with open(filename, mode="r", encoding="utf-8") as f:
-        return json.load(f)
+    """Function that creates an Object from JSON file"""
+    with open(filename, encoding="utf-8") as f:
+        return json.loads(f.read())
