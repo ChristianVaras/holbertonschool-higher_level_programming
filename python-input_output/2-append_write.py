10fe003ce1772be875ffd057eba33c61c360cecd
ChristianVaras <christianvaras.2020@gmail.com>
2024-01-22 16:12:29 -0500

    Adding tasks 9 and 10

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-Module 4-append_write
-
-Contains function that appends to text file and returns num chars added
-"""
+"""Function that appends a string"""
 
 
 def append_write(filename="", text=""):
-    """appends to text file and returns num chars added"""
+    """Function that appends a string at the end
+    of a text file (UTF8) and returns the number
+    of characters added"""
+
     with open(filename, mode="a", encoding="utf-8") as f:
-        return (f.write(text))
+        f.write(text)
+
+    return len(text)
