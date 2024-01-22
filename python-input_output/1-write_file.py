b06449a7512237cf1ecf3f9a743275a44c67a400
ChristianVaras <christianvaras.2020@gmail.com>
2024-01-22 15:53:53 -0500

    Adding tasks 5 and 6

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-Module 3-write_file
-
-Contains function that writes to text file and returns num chars written
-"""
+"""Function that writes a string to a text file"""
 
 
 def write_file(filename="", text=""):
-    """writes to text file and returns num chars written"""
+    """This function that writes a string
+    to a text file and returns the number of characters
+    written"""
+
+    n_characters = 0
+
     with open(filename, mode="w", encoding="utf-8") as f:
-        return (f.write(text))
+        f.write(text)
+
+    n_characters = len(text)
+
+    return n_characters
