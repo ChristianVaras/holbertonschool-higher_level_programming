da59fedaaf10a39771d7afb63874770fffe0370a
ChristianVaras <christianvaras.2020@gmail.com>
2024-01-23 18:52:17 -0500

    Adding tasks 10 and 11

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,2 @@
--- print full description of table; without using 'DECSCRIBE [tableName]'
---
--- Table   Create Table
--- first_table CREATE TABLE `first_table` (\n  `id` int(11) DEFAULT NULL,
--- \n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=latin1
-
-SHOW CREATE TABLE first_table;
+-- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+SHOW CREATE TABLE first_table
